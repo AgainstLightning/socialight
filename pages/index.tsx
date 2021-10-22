663c16a857506ce2d3275dbec83206ea77e14246
Jared <againstlightning@gmail.com>
2021-10-22 14:55:20 -0400

    add Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,10 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <main>
       <h1>HOME</h1>
       <Loader show />
-    </div>
+    </main>
   )
 }
 
