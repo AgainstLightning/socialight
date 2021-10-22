ece208dd039db11124a29951fa6c12a1a7a58e3e
Jared <againstlightning@gmail.com>
2021-10-22 00:49:02 -0400

    add enter page

---
+++ pages/enter.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Enter: NextPage = ({ }) => {
+    return (
+        <main>
+            <h1>SIGN UP</h1>
+        </main>
+    )
+}
+
+export default Enter
