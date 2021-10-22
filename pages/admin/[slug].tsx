6fce6a38a439cc05eb19f7df13c8cc81eb585a14
Jared <againstlightning@gmail.com>
2021-10-22 00:57:44 -0400

    add dynamic admin route

---
+++ pages/admin/[slug].tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Slug: NextPage = ({ }) => {
+    return (
+        <main>
+            <h1>Slug</h1>
+        </main>
+    )
+}
+
+export default Slug
---
+++ pages/admin/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Admin: NextPage = ({ }) => {
+    return (
+        <main>
+            <h1>Admin</h1>
+        </main>
+    )
+}
+
+export default Admin
